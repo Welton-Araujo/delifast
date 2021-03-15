38a2b7caeb1e4157e1906931ab7ae691ed7dbcb4
Welton-Araujo <weltonaraujo.cc@gmail.com>
2021-03-15 14:24:18 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/deliffast/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.deliffast
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import './screens/auth/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlutterFoodApp());
+
+class FlutterFoodApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'FlutterFood',
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(147, 70, 248, 1),
+        backgroundColor: Colors.white,
+        accentColor: Colors.black,
+        brightness: Brightness.dark
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
